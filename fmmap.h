da4563886d1ad29a3d5c3a1613e749f51a0e260b
Widianto Nur Firmansah <xnaltasee@gmail.com>
2022-06-11 01:18:43 +0700

    fmmap_open: remove restrict qualifier
    it's only used if a function is received 2 or more pointer arguments.

--- fmmap.h
+++ fmmap.h
@@ -52,14 +52,14 @@ typedef struct fmmap fmmap;
  * cannot be used on stdin, stdout, and stderr since they are not
  * regular files or devices.
  */
-fmmap *fmmap_open_length(const char *restrict, int, size_t);
+fmmap *fmmap_open_length(const char *, int, size_t);
 
 /* fmmap_open() doesn't work on pseudo filesystem like procfs.
  * 
  * st_size from stat() returns 0 therefore use fmmap_open_length() instead
  * and provide the 'len' manually by yourself.
  */
-fmmap *fmmap_open(const char *restrict, int);
+fmmap *fmmap_open(const char *, int);
 
 size_t fmmap_read(fmmap *restrict, void *restrict, size_t);
 size_t fmmap_write(fmmap *restrict, const void *restrict, size_t);
