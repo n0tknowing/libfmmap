c757727c1f2bd77bb929df749d78ec34bfed87ec
Widianto Nur Firmansah <xnaltasee@gmail.com>
2022-06-16 08:54:22 +0700

    fmmap: use 'copysz' for length
    also update example6

--- examples/example6.c
+++ examples/example6.c
@@ -8,7 +8,7 @@ int main(int argc, char **argv)
 	if (argc < 2)
 		return 1;
 
-	fmmap *fm = fmmap_create(argv[1], FMMAP_RDONLY|FMMAP_APPEND, 0600);
+	fmmap *fm = fmmap_create(argv[1], FMMAP_WRONLY|FMMAP_APPEND, 0600);
 	if (!fm)
 		err(1, "fmmap_create");
 
@@ -16,6 +16,11 @@ int main(int argc, char **argv)
 	printf("off=%zu\n", fmmap_tell(fm));
 	if (fmmap_seek(fm, 0, FMMAP_SEEK_END) < 0)
 		perror("seek fail");
+	printf("off=%zu\n", fmmap_tell(fm));
+
+	if (fmmap_write(fm, "hello\n", 6) != 6)
+		perror("fmmap_write fail");
+	printf("len=%zu\n", fmmap_length(fm));
 	printf("off=%zu\n", fmmap_tell(fm));
 
 	return fmmap_close(fm);
--- fmmap.c
+++ fmmap.c
@@ -224,7 +224,7 @@ size_t fmmap_write(fmmap *restrict fm, const void *restrict ptr, size_t size)
 	}
 
 	const uint8_t *sptr = ptr;
-	size_t count = 0;
+	size_t count = 0, copysz;
 
 	if (!size)
 		return 0;
@@ -236,18 +236,20 @@ size_t fmmap_write(fmmap *restrict fm, const void *restrict ptr, size_t size)
 		return 0;
 
 	while (size > FMMAP_BLOCK_SIZE) {
-		memmove((uint8_t *)fm->addr + fm->curoff, sptr + count, FMMAP_BLOCK_SIZE);
-		fm->curoff += FMMAP_BLOCK_SIZE;
-		size       -= FMMAP_BLOCK_SIZE;
-		count      += FMMAP_BLOCK_SIZE;
+		copysz = MIN(FMMAP_BLOCK_SIZE, fm->mapsz - fm->curoff);
+		memmove((uint8_t *)fm->addr + fm->curoff, sptr + count, copysz);
+		fm->curoff += copysz;
+		size       -= copysz;
+		count      += copysz;
 		if (fmmap_sync(fm, fm->curoff) < 0)
 			return 0;
 	}
 
 	if (size > 0) {
-		memmove((uint8_t *)fm->addr + fm->curoff, sptr + count, size);
-		fm->curoff += size;
-		count      += size;
+		copysz = MIN(size, fm->mapsz - fm->curoff);
+		memmove((uint8_t *)fm->addr + fm->curoff, sptr + count, copysz);
+		fm->curoff += copysz;
+		count      += copysz;
 		if (fmmap_sync(fm, fm->curoff) < 0)
 			return 0;
 	}
