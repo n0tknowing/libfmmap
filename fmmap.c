208019b69e9bb15fac0099094d4ae3db1d6b2426
Widianto Nur F <xnaltasee@gmail.com>
2022-12-24 22:21:09 +0700

    fmmap: reformat
    - use 4 spaces
    - no longer treat NULL like boolean (no !buf, but buf == NULL)

--- fmmap.c
+++ fmmap.c
@@ -52,327 +52,327 @@ static int fmmap_remap(struct fmmap *, size_t);
 static int fmmap_sync(struct fmmap *);
 
 struct fmmap {
-	void *addr;
-	size_t mapsz;
-	size_t length;
-	size_t curoff;
-	int mode;
-	int fd;
+    void *addr;
+    size_t mapsz; /* for writing */
+    size_t length; /* for reading */
+    size_t curoff;
+    int mode;
+    int fd;
 };
 
 struct fmmap *fmmap_open_length(const char *file, int mode, size_t filelen)
 {
-	if (!file || filelen == 0) {
-		errno = EINVAL;
-		return NULL;
-	}
+    if (file == NULL || filelen == 0) {
+        errno = EINVAL;
+        return NULL;
+    }
 
-	void *buf;
-	struct fmmap *fm;
-	struct stat sb;
-	int fd, fd_flag, fm_mode, fm_flag;
-	size_t mapsz = filelen;
+    void *buf;
+    struct fmmap *fm;
+    struct stat sb;
+    int fd, fd_flag, fm_mode, fm_flag;
+    size_t mapsz = filelen;
 
-	if (filelen >= FMMAP_MAX_SIZE) {
-		errno = EOVERFLOW;
-		return NULL;
-	}
+    if (filelen >= FMMAP_MAX_SIZE) {
+        errno = EOVERFLOW;
+        return NULL;
+    }
 
-	if ((mode & FMMAP_WRONLY) == FMMAP_WRONLY) {
-		fd_flag = O_RDWR;
-		fm_mode = PROT_WRITE;
-	} else if ((mode & FMMAP_RDWR) == FMMAP_RDWR) {
-		fd_flag = O_RDWR;
-		fm_mode = PROT_READ | PROT_WRITE;
-	} else if ((mode & FMMAP_RDONLY) == FMMAP_RDONLY) {
-		fd_flag = O_RDONLY;
-		fm_mode = PROT_READ;
-	} else {
-		/* none of three flags above present */
-		errno = EINVAL;
-		return NULL;
-	}
+    if ((mode & FMMAP_WRONLY) == FMMAP_WRONLY) {
+        fd_flag = O_RDWR;
+        fm_mode = PROT_WRITE;
+    } else if ((mode & FMMAP_RDWR) == FMMAP_RDWR) {
+        fd_flag = O_RDWR;
+        fm_mode = PROT_READ | PROT_WRITE;
+    } else if ((mode & FMMAP_RDONLY) == FMMAP_RDONLY) {
+        fd_flag = O_RDONLY;
+        fm_mode = PROT_READ;
+    } else {
+        /* none of three flags above present */
+        errno = EINVAL;
+        return NULL;
+    }
 
-	fd = open(file, fd_flag | O_CLOEXEC);
-	if (fd < 0)
-		return NULL;
+    fd = open(file, fd_flag | O_CLOEXEC);
+    if (fd < 0)
+        return NULL;
 
-	if (fstat(fd, &sb) < 0)
-		goto close_fd;
+    if (fstat(fd, &sb) < 0)
+        goto close_fd;
 
-	if (!S_ISREG(sb.st_mode)) {
-		errno = EINVAL;
-		goto close_fd;
-	}
+    if (!S_ISREG(sb.st_mode)) {
+        errno = EINVAL;
+        goto close_fd;
+    }
 
-	fm_flag = MAP_SHARED;
-	buf = mmap(NULL, mapsz, fm_mode, fm_flag, fd, 0);
-	if (buf == MAP_FAILED)
-		goto close_fd;
+    fm_flag = MAP_SHARED;
+    buf = mmap(NULL, mapsz, fm_mode, fm_flag, fd, 0);
+    if (buf == MAP_FAILED)
+        goto close_fd;
 
-	/* when we encounter FMMAP_TRUNC and FMMAP_APPEND
-	 * at the same time, ignore FMMAP_TRUNC.
-	 */
-	if ((mode & FMMAP_TRUNC)  == FMMAP_TRUNC &&
-	    (mode & FMMAP_APPEND) != FMMAP_APPEND) {
-		if ((mode & FMMAP_RDONLY) == FMMAP_RDONLY) {
-			errno = EPERM;
-			goto delete_map;
-		}
+    /* when we encounter FMMAP_TRUNC and FMMAP_APPEND
+     * at the same time, ignore FMMAP_TRUNC.
+     */
+    if ((mode & FMMAP_TRUNC) == FMMAP_TRUNC &&
+        (mode & FMMAP_APPEND) != FMMAP_APPEND) {
+        if ((mode & FMMAP_RDONLY) == FMMAP_RDONLY) {
+            errno = EPERM;
+            goto delete_map;
+        }
 
-		memset(buf, 0, filelen);
-		filelen = 0;
-	}
+        memset(buf, 0, filelen);
+        filelen = 0;
+    }
 
-	/* ignore these madvise if error */
-	(void)madvise(buf, mapsz, MADV_WILLNEED);
-	(void)madvise(buf, mapsz, MADV_SEQUENTIAL);
+    /* ignore these madvise if error */
+    (void)madvise(buf, mapsz, MADV_WILLNEED);
+    (void)madvise(buf, mapsz, MADV_SEQUENTIAL);
 
-	fm = malloc(sizeof(struct fmmap));
-	if (!fm)
-		goto delete_map;
+    fm = malloc(sizeof(struct fmmap));
+    if (fm == NULL)
+        goto delete_map;
 
-	fm->addr = buf;
-	fm->mapsz = mapsz;
-	fm->length = filelen;
-	fm->mode = mode;
-	fm->curoff = 0;
-	fm->fd = fd;
+    fm->addr = buf;
+    fm->mapsz = mapsz;
+    fm->length = filelen;
+    fm->mode = mode;
+    fm->curoff = 0;
+    fm->fd = fd;
 
-	return fm;
+    return fm;
 
 delete_map:
-	(void)munmap(buf, mapsz);
+    (void)munmap(buf, mapsz);
 
 close_fd:
-	(void)close(fd);
-	return NULL;
+    (void)close(fd);
+    return NULL;
 }
 
 struct fmmap *fmmap_open(const char *file, int mode)
 {
-	struct stat sb;
+    struct stat sb;
 
-	if (stat(file, &sb) < 0)
-		return NULL;
+    if (stat(file, &sb) < 0)
+        return NULL;
 
-	return fmmap_open_length(file, mode, sb.st_size);
+    return fmmap_open_length(file, mode, sb.st_size);
 }
 
 struct fmmap *fmmap_create(const char *filename, int mode, int perms)
 {
-	if (!filename) {
-		errno = EINVAL;
-		return NULL;
-	}
+    if (filename == NULL) {
+        errno = EINVAL;
+        return NULL;
+    }
 
-	int fd;
-	int open_flags = O_RDWR | O_CREAT;
+    int fd;
+    int open_flags = O_RDWR | O_CREAT;
 
-	if ((mode & FMMAP_EXCL) == FMMAP_EXCL)
-		open_flags |= O_EXCL;
+    if ((mode & FMMAP_EXCL) == FMMAP_EXCL)
+        open_flags |= O_EXCL;
 
-	fd = open(filename, open_flags, perms);
-	if (fd < 0)
-		return NULL;
+    fd = open(filename, open_flags, perms);
+    if (fd < 0)
+        return NULL;
 
-	(void)ftruncate(fd, 1);
-	(void)close(fd);
+    (void)ftruncate(fd, 1);
+    (void)close(fd);
 
-	return fmmap_open_length(filename, FMMAP_RDWR | FMMAP_TRUNC, 1);
+    return fmmap_open_length(filename, FMMAP_RDWR | FMMAP_TRUNC, 1);
 }
 
 size_t fmmap_read(fmmap *restrict fm, void *restrict ptr, size_t size)
 {
-	if (!fm || !ptr) {
-		errno = EINVAL;
-		return 0;
-	} else if ((fm->mode & FMMAP_WRONLY) == FMMAP_WRONLY) {
-		errno = EPERM;
-		return 0;
-	}
+    if (fm == NULL || ptr == NULL) {
+        errno = EINVAL;
+        return 0;
+    } else if ((fm->mode & FMMAP_WRONLY) == FMMAP_WRONLY) {
+        errno = EPERM;
+        return 0;
+    }
 
-	uint8_t *dptr = ptr;
-	size_t count = 0;
+    uint8_t *dptr = ptr;
+    size_t count = 0;
 
-	/* It's safe to remove this check, but i prefer this way.
-	 * Another reason is, of course, to avoid memcpy() call with
-	 * just 0 copy size.
-	 */
-	if (fm->curoff < fm->length) {
-		count = MIN(fm->length - fm->curoff, size);
-		memcpy(dptr, (uint8_t *)fm->addr + fm->curoff, count);
-		fm->curoff += count;
-	}
+    /* It's safe to remove this check, but i prefer this way.
+     * Another reason is, of course, to avoid memcpy() call with
+     * just 0 copy size.
+     */
+    if (fm->curoff < fm->length) {
+        count = MIN(fm->length - fm->curoff, size);
+        memcpy(dptr, (uint8_t *)fm->addr + fm->curoff, count);
+        fm->curoff += count;
+    }
 
-	return count;
+    return count;
 }
 
 size_t fmmap_write(fmmap *restrict fm, const void *restrict ptr, size_t size)
 {
-	if (!fm || !ptr) {
-		errno = EINVAL;
-		return 0;
-	} else if ((fm->mode & FMMAP_RDONLY) == FMMAP_RDONLY) {
-		errno = EPERM;
-		return 0;
-	}
+    if (fm == NULL || ptr == NULL) {
+        errno = EINVAL;
+        return 0;
+    } else if ((fm->mode & FMMAP_RDONLY) == FMMAP_RDONLY) {
+        errno = EPERM;
+        return 0;
+    }
 
-	size_t count = 0;
+    size_t count = 0;
 
-	if ((fm->mode & FMMAP_APPEND) == FMMAP_APPEND)
-		fm->curoff = fm->length;
+    if ((fm->mode & FMMAP_APPEND) == FMMAP_APPEND)
+        fm->curoff = fm->length;
 
-	if (fmmap_remap(fm, fm->length + size) < 0)
-		return 0;
+    if (fmmap_remap(fm, fm->length + size) < 0)
+        return 0;
 
-	count = MIN(fm->mapsz - fm->curoff, size);
-	memcpy((uint8_t *)fm->addr + fm->curoff, ptr, count);
-	fm->curoff += count;
+    count = MIN(fm->mapsz - fm->curoff, size);
+    memcpy((uint8_t *)fm->addr + fm->curoff, ptr, count);
+    fm->curoff += count;
 
-	if (fmmap_sync(fm) < 0)
-		return 0;
+    if (fmmap_sync(fm) < 0)
+        return 0;
 
-	return count;
+    return count;
 }
 
 off_t fmmap_seek(struct fmmap *fm, off_t offset, int whence)
 {
-	if (!fm) {
-		errno = EINVAL;
-		return -1;
-	}
+    if (fm == NULL) {
+        errno = EINVAL;
+        return -1;
+    }
 
-	size_t oldoff = fm->curoff;
+    size_t oldoff = fm->curoff;
 
-	/* off_t is signed integer */
-	if (offset < 0 || offset > (off_t)fm->length)
-		goto beyond_size;
+    /* off_t is signed integer */
+    if (offset < 0 || offset > (off_t)fm->length)
+        goto beyond_size;
 
-	switch (whence) {
-	case FMMAP_SEEK_SET:
-		fm->curoff = offset;
-		break;
-	case FMMAP_SEEK_CUR:
-		fm->curoff += offset;
-		if (CUROFF_OVERFLOW(fm))
-			goto beyond_size;
-		break;
-	case FMMAP_SEEK_END:
-		fm->curoff = fm->length - offset;
-		if (CUROFF_OVERFLOW(fm))
-			goto beyond_size;
-		break;
-	default:
-		errno = EINVAL;
-		return -1;
-	}
+    switch (whence) {
+        case FMMAP_SEEK_SET:
+            fm->curoff = offset;
+            break;
+        case FMMAP_SEEK_CUR:
+            fm->curoff += offset;
+            if (CUROFF_OVERFLOW(fm))
+                goto beyond_size;
+            break;
+        case FMMAP_SEEK_END:
+            fm->curoff = fm->length - offset;
+            if (CUROFF_OVERFLOW(fm))
+                goto beyond_size;
+            break;
+        default:
+            errno = EINVAL;
+            return -1;
+    }
 
-	return (off_t)fm->curoff;
+    return (off_t)fm->curoff;
 
 beyond_size:
-	fm->curoff = oldoff;
-	errno = EOVERFLOW;
-	return -1;
+    fm->curoff = oldoff;
+    errno = EOVERFLOW;
+    return -1;
 }
 
 void fmmap_rewind(struct fmmap *fm)
 {
-	int save = errno;
+    int save = errno;
 
-	/* fmmap_seek checks for 'fm' if NULL, so we no need to
-	 * check again
-	 */
-	(void)fmmap_seek(fm, 0, FMMAP_SEEK_SET);
-	errno = save;
+    /* fmmap_seek checks for 'fm' if NULL, so we no need to
+     * check again
+     */
+    (void)fmmap_seek(fm, 0, FMMAP_SEEK_SET);
+    errno = save;
 }
 
 size_t fmmap_tell(struct fmmap *fm)
 {
-	if (!fm) {
-		errno = EINVAL;
-		return 0;
-	}
+    if (fm == NULL) {
+        errno = EINVAL;
+        return 0;
+    }
 
-	return fm->curoff;
+    return fm->curoff;
 }
 
 size_t fmmap_length(struct fmmap *fm)
 {
-	if (!fm) {
-		errno = EINVAL;
-		return 0;
-	}
+    if (fm == NULL) {
+        errno = EINVAL;
+        return 0;
+    }
 
-	return fm->length;
+    return fm->length;
 }
 
 bool fmmap_iseof(struct fmmap *fm)
 {
-	if (!fm)
-		return true;
+    if (fm == NULL)
+        return true;
 
-	return fm->length > 0 && fm->curoff == fm->length;
+    return fm->length > 0 && fm->curoff == fm->length;
 }
 
 int fmmap_close(struct fmmap *fm)
 {
-	if (!fm) {
-		errno = EINVAL;
-		return -1;
-	}
+    if (fm == NULL) {
+        errno = EINVAL;
+        return -1;
+    }
 
-	int r;
+    int r;
 
-	(void)close(fm->fd); /* ignore if error */
-	r = munmap(fm->addr, fm->mapsz);
-	fm->addr = NULL;
-	fm->fd = -1;
-	fm->mapsz = fm->length = fm->mode = fm->curoff = 0;
-	free(fm);
+    (void)close(fm->fd); /* ignore if error */
+    r = munmap(fm->addr, fm->mapsz);
+    fm->addr = NULL;
+    fm->fd = -1;
+    fm->mapsz = fm->length = fm->mode = fm->curoff = 0;
+    free(fm);
 
-	return r;
+    return r;
 }
 
 /* update both mapping size and file size for writing */
 static int fmmap_remap(struct fmmap *fm, size_t newsz)
 {
-	if (newsz <= fm->mapsz)
-		return 0;
+    if (newsz <= fm->mapsz)
+        return 0;
 
-	/* first, truncate file to the desired file size for writing. */
-	if (ftruncate(fm->fd, newsz) < 0)
-		return -1;
+    /* first, truncate file to the desired file size for writing. */
+    if (ftruncate(fm->fd, newsz) < 0)
+        return -1;
 
-	int save = errno;
+    int save = errno;
 
-	/* second, create new mapping.
-	 *
-	 * MREMAP_MAYMOVE to avoid ENOMEM error.
-	 */
-	void *new = mremap(fm->addr, fm->mapsz, newsz, MREMAP_MAYMOVE);
-	if (new == MAP_FAILED)
-		return -1;
+    /* second, create new mapping.
+     *
+     * MREMAP_MAYMOVE to avoid ENOMEM error.
+     */
+    void *new = mremap(fm->addr, fm->mapsz, newsz, MREMAP_MAYMOVE);
+    if (new == MAP_FAILED)
+        return -1;
 
-	/* third, request advise again.
-	 *
-	 * manual page doesn't describe what happen to the previous
-	 * advise after calling mremap.
-	 */
-	(void)madvise(new, newsz, MADV_WILLNEED);
-	(void)madvise(new, newsz, MADV_SEQUENTIAL);
+    /* third, request advise again.
+     *
+     * manual page doesn't describe what happen to the previous
+     * advise after calling mremap.
+     */
+    (void)madvise(new, newsz, MADV_WILLNEED);
+    (void)madvise(new, newsz, MADV_SEQUENTIAL);
 
-	/* last, update our structure. */
-	fm->addr = new;
-	fm->mapsz = newsz;
-	fm->length = newsz;
+    /* last, update our structure. */
+    fm->addr = new;
+    fm->mapsz = newsz;
+    fm->length = newsz;
 
-	errno = save;
-	return 0;
+    errno = save;
+    return 0;
 }
 
 /* synchronize memory with file */
 static int fmmap_sync(struct fmmap *fm)
 {
-	return msync(fm->addr, fm->mapsz, MS_ASYNC);
+    return msync(fm->addr, fm->mapsz, MS_ASYNC);
 }
